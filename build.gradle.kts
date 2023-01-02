a4bde4520504c2bcf2af17e94bbb1f7f76eb00dc
Michele Giannone <mickgian@users.noreply.github.com>
2023-01-02 12:42:03 +0100

    Initialize buildsrc_update

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    val kotlinVersion: String by project
+    println(kotlinVersion)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:${kotlinVersion}")
+        classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutinesVersion}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+        maven(url = "https://androidx.dev/snapshots/builds/7909927/artifacts/repository")
+    }
+}
+
